8f0342b7f61f4bb84fd451cb51cca563be3090ec
Hayden Cordeiro <cordeirohayden@gmail.com>
2023-10-25 19:38:18 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({
+    "data": [
+      {
+        "productName": "test12",
+        "productDescription": "This is the product description",
+        "productPrice": "1200",
+        "productImageUrl": "https://assets.myntassets.com/f_webp,fl_progressive/h_960,q_80,w_720/v1/assets/images/19020748/2022/12/5/d44e7bd2-b582-4af8-87e8-cccd051849cf1670225391472-US-Polo-Assn-Men-Olive-Green-Solid-Padded-Jacket-91416702253-1.jpg"
+      },
+      {
+        "productName": "test12",
+        "productDescription": "This is the product description",
+        "productPrice": "1200",
+        "productImageUrl": "https://assets.myntassets.com/f_webp,fl_progressive/h_960,q_80,w_720/v1/assets/images/19020748/2022/12/5/d44e7bd2-b582-4af8-87e8-cccd051849cf1670225391472-US-Polo-Assn-Men-Olive-Green-Solid-Padded-Jacket-91416702253-1.jpg"
+      },
+      {
+        "productName": "test12",
+        "productDescription": "This is the product description",
+        "productPrice": "1200",
+        "productImageUrl": "https://assets.myntassets.com/f_webp,fl_progressive/h_960,q_80,w_720/v1/assets/images/19020748/2022/12/5/d44e7bd2-b582-4af8-87e8-cccd051849cf1670225391472-US-Polo-Assn-Men-Olive-Green-Solid-Padded-Jacket-91416702253-1.jpg"
+      },
+      {
+        "productName": "test12",
+        "productDescription": "This is the product description",
+        "productPrice": "1200",
+        "productImageUrl": "https://assets.myntassets.com/f_webp,fl_progressive/h_960,q_80,w_720/v1/assets/images/19020748/2022/12/5/d44e7bd2-b582-4af8-87e8-cccd051849cf1670225391472-US-Polo-Assn-Men-Olive-Green-Solid-Padded-Jacket-91416702253-1.jpg"
+      },
+      {
+        "productName": "test12",
+        "productDescription": "This is the product description",
+        "productPrice": "1200",
+        "productImageUrl": "https://assets.myntassets.com/f_webp,fl_progressive/h_960,q_80,w_720/v1/assets/images/19020748/2022/12/5/d44e7bd2-b582-4af8-87e8-cccd051849cf1670225391472-US-Polo-Assn-Men-Olive-Green-Solid-Padded-Jacket-91416702253-1.jpg"
+      },
+      {
+        "productName": "test12",
+        "productDescription": "This is the product description",
+        "productPrice": "1200",
+        "productImageUrl": "https://assets.myntassets.com/f_webp,fl_progressive/h_960,q_80,w_720/v1/assets/images/19020748/2022/12/5/d44e7bd2-b582-4af8-87e8-cccd051849cf1670225391472-US-Polo-Assn-Men-Olive-Green-Solid-Padded-Jacket-91416702253-1.jpg"
+      },
+      {
+        "productName": "test12",
+        "productDescription": "This is the product description",
+        "productPrice": "1200",
+        "productImageUrl": "https://assets.myntassets.com/f_webp,fl_progressive/h_960,q_80,w_720/v1/assets/images/19020748/2022/12/5/d44e7bd2-b582-4af8-87e8-cccd051849cf1670225391472-US-Polo-Assn-Men-Olive-Green-Solid-Padded-Jacket-91416702253-1.jpg"
+      },
+      {
+        "productName": "test12",
+        "productDescription": "This is the product description",
+        "productPrice": "1200",
+        "productImageUrl": "https://assets.myntassets.com/f_webp,fl_progressive/h_960,q_80,w_720/v1/assets/images/19020748/2022/12/5/d44e7bd2-b582-4af8-87e8-cccd051849cf1670225391472-US-Polo-Assn-Men-Olive-Green-Solid-Padded-Jacket-91416702253-1.jpg"
+      },
+      {
+        "productName": "test12",
+        "productDescription": "This is the product description",
+        "productPrice": "1200",
+        "productImageUrl": "https://assets.myntassets.com/f_webp,fl_progressive/h_960,q_80,w_720/v1/assets/images/19020748/2022/12/5/d44e7bd2-b582-4af8-87e8-cccd051849cf1670225391472-US-Polo-Assn-Men-Olive-Green-Solid-Padded-Jacket-91416702253-1.jpg"
+      },
+      {
+        "productName": "test12",
+        "productDescription": "This is the product description",
+        "productPrice": "1200",
+        "productImageUrl": "https://assets.myntassets.com/f_webp,fl_progressive/h_960,q_80,w_720/v1/assets/images/19020748/2022/12/5/d44e7bd2-b582-4af8-87e8-cccd051849cf1670225391472-US-Polo-Assn-Men-Olive-Green-Solid-Padded-Jacket-91416702253-1.jpg"
+      }
+    ]
+  })
+
+})
+app.listen(3000)
